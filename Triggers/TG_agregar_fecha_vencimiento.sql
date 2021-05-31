fc8028115937ecac4697d1078b6009a8a26d4777
JuanIGerstner <jiger360@gmail.com>
2021-05-30 22:08:41 -0300

    se añadio un trigger para la fecha de vencimiento
    TG_agregar_fecha_vencimiento

---
+++ Triggers/TG_agregar_fecha_vencimiento.sql
@@ -0,0 +1,36 @@
+
+CREATE OR REPLACE FUNCTION SP_agregar_fecha_vencimiento()
+    RETURNS TRIGGER AS 
+    $$
+		DECLARE
+		 va_fecha_vencimiento integer;
+		 va_fecha date;
+		 va_ultimo_dia_fecha date;
+		 
+        BEGIN
+				--obtenemos el dia del mes de vencimiento de la cuota segun el contrato
+				SELECT ca.vencimiento_cuota INTO va_fecha_vencimiento FROM contratoalquiler AS ca
+					WHERE ca.id_inmueble = new.id_inmueble AND ca.id_cliente = new.id_cliente;
+				-- convertimos nuestro mes y año de la cuota en DATE	
+				va_fecha := SP_convertir_mesaño_date(new.mesaño);
+				-- creamos una variable con el ultimo dia del mes
+				va_ultimo_dia_fecha:=  (date_trunc('month', va_fecha) + interval '1 month') - interval '1 day';
+				-- verificamos que el dia de vencimiento sea menor al ultimo dia del mes
+				IF (cast(extract(day from va_ultimo_dia_fecha) as integer)) <= va_fecha_vencimiento THEN
+					--si es mayor asignamos el ultimo dia del mes a la fecha de vencimiento de la cuota
+					va_fecha_vencimiento:=cast(extract(day from va_ultimo_dia_fecha) as integer);
+				END IF;
+				-- creamos la fecha de vencimiento del mes proximo
+				new.fechavencimiento := DATE( cast (va_fecha_vencimiento as varchar) || '-' || cast((va_fecha + interval '1 MONTH') as varchar ));
+
+			RETURN new;
+        END;
+    $$
+    LANGUAGE plpgsql;
+
+CREATE TRIGGER TG_agregar_fecha_vencimiento
+    before insert or update
+    ON cuotas
+    FOR EACH ROW 
+    EXECUTE PROCEDURE SP_agregar_fecha_vencimiento();
+	

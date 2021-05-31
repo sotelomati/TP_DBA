92467326e37130957433aa5b4aa81685184cc91e
JuanIGerstner <jiger360@gmail.com>
2021-05-30 23:24:50 -0300

    Update TG_agregar_fecha_vencimiento.sql

--- Triggers/TG_agregar_fecha_vencimiento.sql
+++ Triggers/TG_agregar_fecha_vencimiento.sql
@@ -14,14 +14,15 @@ CREATE OR REPLACE FUNCTION SP_agregar_fecha_vencimiento()
 				-- convertimos nuestro mes y año de la cuota en DATE	
 				va_fecha := SP_convertir_mesaño_date(new.mesaño);
 				-- creamos una variable con el ultimo dia del mes
-				va_ultimo_dia_fecha:=  (date_trunc('month', va_fecha) + interval '1 month') - interval '1 day';
+				va_ultimo_dia_fecha:=  DATE((date_trunc('month', va_fecha) + interval '2 month') - interval '1 day');
 				-- verificamos que el dia de vencimiento sea menor al ultimo dia del mes
 				IF (cast(extract(day from va_ultimo_dia_fecha) as integer)) <= va_fecha_vencimiento THEN
 					--si es mayor asignamos el ultimo dia del mes a la fecha de vencimiento de la cuota
 					va_fecha_vencimiento:=cast(extract(day from va_ultimo_dia_fecha) as integer);
+					
 				END IF;
 				-- creamos la fecha de vencimiento del mes proximo
-				new.fechavencimiento := DATE( cast (va_fecha_vencimiento as varchar) || '-' || cast((va_fecha + interval '1 MONTH') as varchar ));
+				new.fechavencimiento := DATE( cast (va_fecha_vencimiento as varchar) || '-' || sp_convertir_date_mesaño((va_fecha + interval '1 MONTH')::date));
 
 			RETURN new;
         END;

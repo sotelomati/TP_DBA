76cd816307b38a4e35224e3fc81fa351c5f53fc7
GerstnerJuanI <43797469+GerstnerJuanI@users.noreply.github.com>
2021-06-14 16:47:22 -0300

    Create E_Empleados_Dependientes.sql

---
+++ Ejercicios/Ejercicio4/E_Empleados_Dependientes.sql
@@ -0,0 +1,29 @@
+create or replace function empleados_dependientes(cuit) returns setof "record"
+as $$
+    DECLARE
+    e record;
+    e2 record;
+    BEGIN
+        for e in select emp.cuit, emp.apellido_nombre,emp.fecha_ingreso, emp.cargo, emp.superior as depende
+                from empleados as emp
+                where emp.superior = $1
+            LOOP
+                    for e2 in select cuit,apellido_nombre,fecha_ingreso, cargo, depende 
+                        from empleados_dependientes(e.cuit) as (cuit cuit, apellido_nombre varchar(70),fecha_ingreso date, cargo varchar(15), depende cuit)
+                        LOOP
+                            return next e2;
+                        end LOOP;
+                return next e;
+            end LOOP;
+            return;
+    end;
+    $$
+LANGUAGE plpgsql;
+
+--Ejemplo de consulta de la funcion 
+	select * from empleados_dependientes('23385703139') 
+	as (cuit cuit, apellido_nombre varchar(70),fecha_ingreso date, cargo varchar(15), depende cuit)
+	order by depende
+
+
+

3b184dc49c0bf011c20b060f90c359fd76b003f1
Your Name <you@example.com>
2021-06-14 23:28:32 -0300

    agrego doc de entrega y trigger que evita el alta de contrato en un inmueble con contrato activo

---
+++ Triggers/TG_validar_integridad_contrato.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE FUNCTION SP_validar_integridad_contrato()
+RETURNS TRIGGER AS
+$$
+BEGIN
+	
+	IF NOT EXISTS Select * from contratoAlquiler 
+				  where id_inmueble= NEW.idinmueble AND id_estado = 1 
+	THEN
+		RETURN NEW;
+	END IF;
+
+RAISE NOTICE 'Ya existe un contrato que esta activo para este inmueble';
+RETURN NULL;
+
+END;
+$$ LANGUAGE PLPGSQL;
+
+CREATE TRIGGER TG_validar_integridad_contrato
+BEFORE INSERT ON contratoAlquiler
+FOR EACH ROW
+EXECUTE PROCEDURE SP_validar_integridad_contrato();
